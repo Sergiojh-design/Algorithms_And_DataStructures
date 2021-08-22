d7d51a0d541fa82fc5480e1cfcf02710aba1cc8a
Sergio Herrera <sergio.herrera.323@gmail.com>
2021-08-21 17:32:49 -0700

    Implemented power function using recursion and renamed files.

--- Recursion/-collectOddValuesPR.js
+++ Recursion/-collectOddValuesPR.js
@@ -1,8 +1,14 @@
 //Pure Recursion
+
 /*
- For arrays, use methods like slice, the spread operator, and concat
- that make copies of arrays so you do not mutate them,
+ - For arrays, use methods like slice, the spread operator, and concat
+ that make copies of arrays so you do not mutate them.
+  - Remember that strings are immutable so you will need to use
+ methods like slice, substr, or substring to make copies of strings.
+ - To make copies of objects use Object.assign, or the spread
+ operator.
 */
+
 function collectOddValues(arr){
   let newArr = [];
 
---
+++ Recursion/10-power.js
@@ -0,0 +1,21 @@
+/*
+Write a function called power which accpets a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents.
+*/
+
+/*
+Examples
+power(2, 0) //1
+power(2, 2) //4
+power(2, 4) //16
+*/
+
+function power(base, exp){
+  if(exp === 0){
+    return 1;
+  }
+
+  if(exp === 1){
+    return base;
+  }
+  return base * power(base, exp - 1);
+}
