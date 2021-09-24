8b4c2fdae703bb3cfddc1555e2901b4b61a49159
Sergio Herrera <sergio.herrera.323@gmail.com>
2021-09-24 11:23:46 -0700

    Implement bubbleSort.

---
+++ 6. Bubble Sort/bubbleSort.js
@@ -0,0 +1,51 @@
+/*
+BubbleSort
+- A sorting algorithm where the largest values bubble up to the top!
+*/
+
+//Non opimized, continue sorting even when there are no sorts to be made.
+function bubbleSort(arr){
+  for(let i = arr.length; i>= 0; i--){
+    for(let j = 0; j > i - 1; j++){
+        let temp = arr[j]
+        if(arr[j] > arr[j+1]){
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+    }
+  }
+  return arr;
+}
+
+//Another way of swapping
+function bubbleSort(arr){
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--){
+    for(let j = 0; j < i-1; j++){
+      if(arr[j] > arr[j+1]){
+        swap(arr, j, j+1);
+      }
+    }
+  }
+}
+
+//Optimized if no Swaps are made break the iteration and return sorted array.
+function bubbleSort(arr){
+  let noSwaps;
+  for(let i = arr.length; i>= 0; i--){
+    noSwaps = true;
+    for(let j = 0; j > i - 1; j++){
+        let temp = arr[j]
+        if(arr[j] > arr[j+1]){
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          noSwaps = false;
+        }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
