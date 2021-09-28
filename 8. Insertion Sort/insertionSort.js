200397e42c9299dc31c6a71c93c2351a2401daea
Sergio Herrera <sergio.herrera.323@gmail.com>
2021-09-27 21:06:49 -0700

    Complete insertionSort.

---
+++ 8. Insertion Sort/insertionSort.js
@@ -0,0 +1,32 @@
+/*
+Insertion Sort
+- Builds up the sort by gradually creating a larger half which is always sorted.
+*/
+
+//My Solution
+function insertionSort(arr){
+  for(let i = 1; i < arr.length; i++){
+    let currentVal = arr[i];
+    for(let j = i - 1; j >= 0; j--){
+      if(currentVal < arr[j]){
+        arr[j+1] = arr[j]
+        arr[j] = currentVal;
+        console.log(arr);
+      }
+    }
+  }
+  return arr;
+}
+
+//Udemy Solution
+function insertionSort(arr){
+	var currentVal;
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal;
+    }
+    return arr;
+}
